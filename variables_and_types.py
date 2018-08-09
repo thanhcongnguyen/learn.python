b0b21d3e377c366b988fff6645895c0db3ae31bf
thanhcongnguyen <nguyenthanhcong@unimarmores.com>
2018-08-10 01:45:47 +0700

    variable and types

---
+++ variables_and_types.py
@@ -0,0 +1,25 @@
+#int
+intNumber = 7;
+print('int number: %d' %intNumber);
+
+if isinstance(intNumber, int):
+    print("typeof intNumber is int");
+
+
+#float
+floatNumber = 7.5;
+print('float Number: %f' % floatNumber);
+
+#strings: single quote or double quote
+
+myName = 'nguyenthanhcong';
+print('my name is %s' % myName);
+
+a,b = 3,4;
+print(a,b);
+
+#change value of variable
+myString = None;
+
+myString = "imissyou";
+print(myString);
