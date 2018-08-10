e1c88feeb26ecd5e515d81e4ae8019829b8b8c53
thanhcongnguyen <nguyenthanhcong@unimarmores.com>
2018-08-10 11:08:34 +0700

    create condition

---
+++ condition.py
@@ -0,0 +1,27 @@
+name = "cong1";
+name2 = "cong";
+age = 25;
+if name=="cong" and age==25:
+    print("I'm %s. I %d years old." % (name,age));
+
+
+arrayName = ["cong", "thanh"];
+
+
+#in
+if name in arrayName:
+    print("your name is %s" % name)
+elif age == 255:
+    print("I'm %d" % age)
+else: 
+    print("wellcome to python 3")
+
+if name is name2:
+    print('true')
+else:
+    print('false');
+
+a = 11;
+b = 3;
+
+print(a // b);
